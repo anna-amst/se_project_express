27590edca2625d9171944e20884879e8b7568e66
Anna Vasileva <vasilieva.a@icloud.com>
2024-08-11 11:27:47 -0400

    fix per review

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -12,61 +12,37 @@ const createItem = (req, res) => {
       if (error.name === "ValidationError") {
         res
           .status(BAD_REQUEST)
-          .send({ message: "Validation Error", error: error.message });
+          .send({ message: "Validation Error" });
       } else {
         res
           .status(INTERNAL_SERVER_ERROR)
-          .send({ message: "Internal Server Error", error: error.message });
+          .send({ message: "An error has occurred on the server" });
       }
     });
 };
 
 const getItems = (req, res) => {
   ClothingItem.find({})
-    .then((items) => res.status(200).send(items))
+    .then((items) => res.send(items))
     .catch((err) => {
       console.error(err);
-      return res.status(INTERNAL_SERVER_ERROR).send({ message: err.message });
+      return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
     });
 };
 
-// const updateItem = (req, res) => {
-//   const { itemId } = req.params;
-//   const { imageUrl } = req.body;
-
-//   ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
-//   .orFail(() => {
-//     const error = new Error("Item not found");
-//     error.statusCode = NOT_FOUND;
-//     throw error;
-//   })
-//   .then((item) => res.send({ data: item }))
-//   .catch((err) => {
-//     console.error(`Error ${err.name} with message ${err.message}`);
-
-//     if (err.name === 'ValidationError') {
-//       return res.status(BAD_REQUEST).send({ message: "Invalid data provided" });
-//     }
-
-//     if (err.name === "DocumentNotFoundError") {
-//       return res.status(NOT_FOUND).send({ message: err.message });
-//     }
-
-//     res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
-//   });
-// };
 
 const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
   ClothingItem.findByIdAndDelete(itemId)
   .orFail()
-  .then(() => res.status(200).send({}))
+  .then(() => res.send({
+    message: 'Item successfully deleted'}))
   .catch((err) => {
     console.error(`Error ${err.name} with message ${err.message}`);
 
     if (err.name === "DocumentNotFoundError") {
-      return res.status(NOT_FOUND).send({ message: err.message });
+      return res.status(NOT_FOUND).send({ message: "Document not found" });
     }
     if (err.name === "CastError") {
       return res.status(BAD_REQUEST).send({message: "Invalid data"})
@@ -85,19 +61,19 @@ const likeItem = (req, res) => {
 )
 .orFail()
 .then((item) => {
-  res.status(200).send({ data: item });
+  res.send({ data: item });
 })
 .catch((err) => {
   console.error(`Error ${err.name} with message ${err.message}`);
 
   if (err.name === "DocumentNotFoundError") {
-    return res.status(NOT_FOUND).send({ message: err.message });
+    return res.status(NOT_FOUND).send({ message: "Document not found" });
   }
   if (err.name === "CastError") {
     return res.status(BAD_REQUEST).send({message: "Invalid data"})
   }
 
-  return res.status(INTERNAL_SERVER_ERROR).send({ message: err.message });
+  return res.status(INTERNAL_SERVER_ERROR).send({ message: "An error has occurred on the server" });
 });
 }
 
@@ -109,13 +85,13 @@ const dislikeItem = (req, res) => {
 )
 .orFail()
 .then((item) => {
-  res.status(200).send({ data: item });
+  res.send({ data: item });
 })
 .catch((err) => {
   console.error(`Error ${err.name} with message ${err.message}`);
 
   if (err.name === "DocumentNotFoundError") {
-    return res.status(NOT_FOUND).send({ message: err.message });
+    return res.status(NOT_FOUND).send({ message: "Document not found" });
   }
   if (err.name === "CastError") {
     return res.status(BAD_REQUEST).send({message: "Invalid data"})
